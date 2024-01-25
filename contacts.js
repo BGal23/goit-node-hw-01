fabd53041cabb7b0cac668e2e0004f2c8374a264
Bartłomiej Gal <bgal23@bartomiejs-mbp.home>
2024-01-25 21:40:48 +0100

    finish

---
+++ contacts.js
@@ -0,0 +1,92 @@
+const { readFile, writeFile } = require("fs/promises");
+//const { nanoid } = require("nanoid");
+require("colors");
+
+const contactsArray = () => {
+  return readFile("./db.txt", "utf-8")
+    .then((data) => JSON.parse(data))
+    .catch((err) => console.error(err.message.red));
+};
+
+const saveContactArray = (contacts) => {
+  writeFile("./db.txt", JSON.stringify(contacts, null, 2), (err) => {
+    if (err) {
+      console.error(err.message.red);
+    }
+  });
+};
+
+function listContacts() {
+  contactsArray()
+    .then((contacts) => {
+      return contacts.map((contact) => {
+        return {
+          Name: contact.name,
+          Phone: contact.phone,
+          Email: contact.email,
+        };
+      });
+    })
+    .then((result) => console.table(result))
+    .catch((err) => console.error(err.message.red));
+}
+
+function getContactById(contactId) {
+  contactsArray()
+    .then((contacts) => {
+      return contacts.find((user) => user.id === contactId);
+    })
+    .then((result) =>
+      console.log(
+        `Your searched contact is\n${result.name}\n${result.phone}\n${result.email}`
+          .cyan
+      )
+    )
+    .catch((err) => console.error(err.message.red));
+}
+
+async function removeContact(contactId) {
+  contactsArray()
+    .then((contacts) => {
+      const index = contacts.findIndex((user) => user.id === contactId);
+      const deleteContact = contacts.splice(index, 1);
+      saveContactArray(contacts);
+
+      return deleteContact[0];
+    })
+    .then((result) =>
+      console.log(
+        `Your deleted is contact\n${result.name}\n${result.phone}\n${result.email}`
+          .yellow
+      )
+    )
+    .catch((err) => console.error(err.message.red));
+}
+
+function addContact(name, email, phone) {
+  contactsArray()
+    .then((contacts) => {
+      contacts.push({
+        id: "nanoid()", // mam problem z importem biblioteki
+        name,
+        email,
+        phone,
+      });
+      saveContactArray(contacts);
+      return contacts[contacts.length - 1];
+    })
+    .then((result) =>
+      console.log(
+        `Your added contact is\n${result.name}\n${result.phone}\n${result.email}`
+          .blue
+      )
+    )
+    .catch((err) => console.error(err.message.red));
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("its work");
+const { Command } = require("commander");
+const program = new Command();
+const path = require("path");
+
+const baseDir = "/Users/bgal23/Documents/GitHub/goit-node-hw-01";
+const fileName = "contacts.js";
+const contactsPath = path.join(baseDir, fileName);
+
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require(contactsPath);
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
